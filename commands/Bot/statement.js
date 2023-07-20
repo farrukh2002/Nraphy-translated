23a8abe19fd8c4cddcde4d6a8f5265ffd0c926c9
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-07-20 21:58:08 +0530

    Update statement.js

--- commands/Bot/statement.js
+++ commands/Bot/statement.js
@@ -85,7 +85,7 @@ module.exports = {
       },
       fields: [
         {
-          name: '**»** 'Notice',
+          name: '**»** Notice',
           value: "```" + suggestion + "```",
         },
       ],
