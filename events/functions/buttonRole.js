9371811e57f65964ae68857690848bb834aab658
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-07-20 16:21:13 +0530

    Update buttonRole.js

--- events/functions/buttonRole.js
+++ events/functions/buttonRole.js
@@ -24,8 +24,8 @@ module.exports = async (client, interaction, guildData) => {
             embeds: [
               {
                 color: client.settings.embedColors.red,
-                title: '**»** Buton-Rol Rolü Bulunamadığı İçin Buton-Rol Sıfırlandı!',
-                description: `**•** Tekrar ayarlamak için \`/buton-rol\` komutunu kullanabilirsiniz.`
+                title: '**»** Button-Role Reset Because Button-Role Role Could Not Be Found!',
+                description: `**•** You can use \`/button-role\` command to set it up again.`
               }
             ],
             components: []
@@ -39,8 +39,8 @@ module.exports = async (client, interaction, guildData) => {
             embeds: [
               {
                 color: client.settings.embedColors.red,
-                title: '**»** Buton-Rol Rolü Bulunamadığı İçin Bu Butonu Kullanamazsın!',
-                description: `**•** Farklı bir butona tıkla. Ben senin yerine bu butonu sileceğim.`
+                title: '**»** You Cannot Use This Button Because The Button-Role Role Is Not Found!',
+                description: `**•** Click a different button.  I will delete this button for you.`
               }
             ],
           });
@@ -49,14 +49,14 @@ module.exports = async (client, interaction, guildData) => {
 
       } else if (interaction.guild.roles.cache.get(buttonRoleButtonRoleId).rawPosition >= interaction.guild.members.me.roles.highest.rawPosition) {
 
-        client.logger.log(`Buton-Rol rolünü verecek yetkim bulunmadığı için buton-rol mesajına not bırakılıyor... • ${interaction.guild.name} (${interaction.guild.id})`);
+        client.logger.log(`A note is left on the button-role message because I do not have the authority to assign the button-role role... • ${interaction.guild.name} (${interaction.guild.id})`);
 
         await interaction.message.edit({
           embeds: [
             {
               color: client.settings.embedColors.red,
-              title: '**»** Buton-Rol Rolünü Veremiyorum!',
-              description: `**•** ${interaction.guild.roles.cache.get(buttonRoleButtonRoleId)} rolünün üstünde bir rolüm bulunmalı! Gerekli ayarlamaları yaptıktan sonra alttaki butona tıklayarak sistemi kaldığı yerden devam ettirebilirsiniz.`
+              title: '**»** I Cannot Cast the Button-Role Role!',
+              description: `**•** I must have a role above ${interaction.guild.roles.cache.get(buttonRoleButtonRoleId)}!  After making the necessary adjustments, you can continue the system from where it left off by clicking the button below.`
             }
           ]
         });
@@ -68,12 +68,12 @@ module.exports = async (client, interaction, guildData) => {
         if (interaction.member.roles.cache.has(buttonRoleButtonRoleId)) {
 
           interaction.member.roles.remove(buttonRoleButtonRoleId);
-          interaction.reply({ content: `<@&${buttonRoleButtonRoleId}> rolünü geri aldım!`, ephemeral: true });
+          interaction.reply({ content: `<@&${buttonRoleButtonRoleId}> I got the role back!`, ephemeral: true });
 
         } else {
 
           interaction.member.roles.add(buttonRoleButtonRoleId);
-          interaction.reply({ content: `<@&${buttonRoleButtonRoleId}> rolünü verdim!`, ephemeral: true });
+          interaction.reply({ content: `<@&${buttonRoleButtonRoleId}> I gave the role!`, ephemeral: true });
 
         }
 
@@ -95,7 +95,7 @@ module.exports = async (client, interaction, guildData) => {
           let embed = {
             color: client.settings.embedColors.default,
             author: {
-              name: `${client.user.username} • Buton Rol (Beta)`,
+              name: `${client.user.username} • Button Role (Beta)`,
               icon_url: client.settings.icon,
             },
             /*fields: [
@@ -106,7 +106,7 @@ module.exports = async (client, interaction, guildData) => {
                 },
             ],*/
             footer: {
-              text: `Toplam ${totalClicks} kez tıklandı.`,
+              text: `Clicked a total of ${totalClicks} times.`,
               //icon_url: 'https://i.imgur.com/AfFp7pu.png',
             },
           };
@@ -121,7 +121,7 @@ module.exports = async (client, interaction, guildData) => {
 
             if (!buttonRole) {
 
-              client.logger.error("rol bulunamadı");
+              client.logger.error("role not found");
 
             } else {
 
@@ -137,12 +137,12 @@ module.exports = async (client, interaction, guildData) => {
           }
 
           if (!messageRoles.length) {
-            client.logger.error("bu mesajdaki hiçbir rol durmuyo");
+            client.logger.error("no role in this message");
           }
 
           embed.description = embedComponents.length == 1 ?
-            `**•** ${messageRoles[0]} rolünü almak için aşağıdaki butona tıkla!` :
-            `**•** ${messageRoles.join(', ')} rollerinden birini almak için aşağıdaki butonlara tıkla!`;
+            `**•** Click the button below to get the ${messageRoles[0]} role!` :
+             `**•** Click the buttons below to get one of the ${messageRoles.join(', ')} roles!`;
 
           if (buttonRoleMessage.title) embed.title = `**»** ${buttonRoleMessage.title}`;
 
