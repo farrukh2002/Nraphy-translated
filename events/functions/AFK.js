beb34d1c4931a32f4d3817a2aecd4ef0719a9ce7
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-07-20 07:16:24 +0530

    Update AFK.js

--- events/functions/AFK.js
+++ events/functions/AFK.js
@@ -5,29 +5,29 @@ module.exports.removeAFK = async (client, message, userData) => {
   if (userData?.AFK?.time) {
     const { time, reason } = userData.AFK;
 
-    let sürecik = Date.now() - time;
+    let process = Date.now() - time;
 
-    if (sürecik > 1000) {
+    if (process > 1000) {
 
-      let zaman = humanize(sürecik, { language: "tr", round: true, conjunction: ", ", serialComma: false });
+      let zaman = humanize(process, { language: "en", round: true, conjunction: ", ", serialComma: false });
 
-      if (message.member.moderatable) message.member.setNickname(message.member.displayName.replace("[AFK]", ""), ["Nraphy AFK Sistemi"]);
+      if (message.member.moderatable) message.member.setNickname(message.member.displayName.replace("[AFK]", ""), ["Nraphy AFK System"]);
 
       message.reply({
         embeds: [
           {
             color: client.settings.embedColors.default,
             author: {
-              name: `${message.author.username}, artık AFK değil!`,
+              name: `${message.author.username}, not AFK anymore!`,
               icon_url: message.author.avatarURL(),
             },
             fields: [
               {
-                name: '**»** AFK Olma Sebebi',
+                name: '**»** Reason for AFK',
                 value: `**•** ${reason}`,
               },
               {
-                name: '**»** AFK Olma Süresi',
+                name: '**»** Time to AFK',
                 value: `**•** ${zaman}`,
               },
             ],
@@ -53,17 +53,17 @@ module.exports.userIsAFK = async (client, message, userData) => {
       {
         color: client.settings.embedColors.red,
         author: {
-          name: `${user.username} kullanıcısı şu an AFK!`,
+          name: `${user.username} user is now AFK!`,
           icon_url: user.avatarURL(),
         },
         fields: [
           {
-            name: '**»** AFK Olma Sebebi',
+            name: '**»** Reason for AFK',
             value: `**•** ${reason}`,
           },
           {
-            name: '**»** AFK Olma Süresi',
-            value: `**•** ${humanize(Date.now() - time, { language: "tr", round: true, conjunction: ", ", serialComma: false })}`,
+            name: '**»** Time to AFK',
+            value: `**•** ${humanize(Date.now() - time, { language: "en", round: true, conjunction: ", ", serialComma: false })}`,
           },
         ],
       }
