cc9e9234576dc18933fad74888667aa7fbbd1397
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-07-20 07:06:23 +0530

    Update Date.js

--- modules/Date.js
+++ modules/Date.js
@@ -3,18 +3,18 @@ const moment = require("moment");
 exports.timestamp = function() {
 
     var Months = {
-        "01": "Ocak",
-        "02": "Şubat",
-        "03": "Mart",
-        "04": "Nisan",
-        "05": "Mayıs",
-        "06": "Haziran",
-        "07": "Temmuz",
-        "08": "Ağustos",
-        "09": "Eylül",
-        "10": "Ekim",
-        "11": "Kasım",
-        "12": "Aralık"
+         "01": "January",
+         "02": "February",
+         "03": "March",
+         "04": "April",
+         "05": "May",
+         "06": "June",
+         "07": "July",
+         "08": "August",
+         "09": "September",
+         "10": "October",
+         "11": "November",
+         "12": "December"
     };
 
     return `[${moment(Date.now()).format('DD')} ${Months[moment(Date.now()).format('MM')]} ${moment(Date.now()).format('YYYY')} ${moment(Date.now()).format('• HH:mm:ss')}]`;
